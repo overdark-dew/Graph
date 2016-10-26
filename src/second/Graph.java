d19404b9ec8f0ba5616e9f17d19d3831f4948706
Overdark <Overdark@192.168.10.96>
2016-10-26 10:16:32 +0300

    Commit update 1

--- src/second/Graph.java
+++ src/second/Graph.java
@@ -4,76 +4,104 @@ import java.util.Random;
 
 public class Graph {
 
-	public static void main(String[] args) {
+	private int block = 99;
+	private int n;
 
-		int block = 99;
-		int n = 10;
+	int[][] graph;// = new int[n][n]; // Начальный массив с расстояниями
 
-		int[][] g = new int[n][n]; // Начальный массив с расстояниями
-		int[][] d = g; // Массив для экспериментов
-		int[][] m = new int[n][n]; // Массив с последней точкой,
-									// через которую был учлучшен путь
+	int[][] march;// = new int[n][n]; // Массив с последней точкой, через
+					// которую был учлучшен путь
 
-		// int[][] g = { { block, 0, 0, 0, 0 },
-		// { 9, block, 0, 0, 0 },
-		// { block, 4, block, 0, 0 },
-		// { 7, block, 5, block, 0 },
-		// { block, 15, 3, 2, block } };
+	Graph() {
 
-		// System.out.println(g);
+		n = 10;
+		graph = new int[n][n];
+		march = new int[n][n];
+	}
+
+	Graph(int k) {
+
+		n = k;
+		graph = new int[n][n];
+		march = new int[n][n];
+	}
+
+	int[][] buildRandomGraph() { // метод для генерации графа со случайными
+									// расстояниями
+
 		Random r = new Random();
 
 		for (int i = 0; i < n; ++i) {
-			System.out.println();
+			// System.out.println();
 			for (int j = 0; j < n; ++j) {
 
 				if (j <= i) {
-					System.out.print("XХ   ");
+					// System.out.print("XХ ");
 					continue;
 				} else if (r.nextInt(2) > 0) {
-					g[i][j] = r.nextInt(50) + 1;
+					graph[i][j] = r.nextInt(50) + 1;
 				} else {
-					g[i][j] = block;
+					graph[i][j] = block;
 				}
-				g[j][i] = g[i][j];
-				System.out.print(g[i][j] + "   ");
+				graph[j][i] = graph[i][j];
+				// System.out.print(graph[i][j] + " ");
 			}
 		}
+		return graph;
+	};
 
-		for (int i = 0; i < n; ++i) {
-			System.out.println();
-			for (int j = 0; j < n; ++j) {
-				System.out.println();
-				for (int k = 0; k < n; ++k) {
+	int[][] buildUsersGraph() {
+		return graph;
+		// создать
+		// int[][] g = {
+		// { block, 0, 0, 0, 0 },
+		// { 9, block, 0, 0, 0 },
+		// { block, 4, block, 0, 0 },
+		// { 7, block, 5, block, 0 },
+		// { block, 15, 3, 2, block }
+		// };
+		// System.out.println(g);
+	};
 
-					if (k <= j) {
-						System.out.print("XХ   ");
-						continue;
-					} else if (d[j][k] > d[j][i] + d[i][k]) {
-						d[j][k] = d[j][i] + d[i][k];
-						m[j][k] = i;
-					}
-					if (g[j][k] < 10) {
-						System.out.print(" " + g[j][k] + "   ");
-					} else {
-						System.out.print(g[j][k] + "   ");
-					}
-				}
-			}
-		}
+	void printGraph(int d[][]) {
+		// Напечатать граф
+
 		System.out.println();
 		for (int i = 0; i < n; ++i) {
 			System.out.println();
 			for (int j = 0; j < n; ++j) {
 
-				if (j <= i) {
-					System.out.print("XХ   ");
-					continue;
+				if (d[i][j] > 9) {
+					// System.out.print("XХ ");
+					// continue;
+					System.out.print(d[i][j] + " | ");
+				} else {
+					System.out.print(" " + d[i][j] + " | ");
 				}
-				System.out.print(" " + m[i][j] + "   ");
 
 			}
 
 		}
-	}
+	};
+
+	int[][] findShortWay(int[][] graph) {
+
+		for (int i = 0; i < n; ++i) {
+
+			for (int j = 0; j < n; ++j) {
+
+				for (int k = 0; k < n; ++k) {
+
+					if (graph[j][k] > graph[j][i] + graph[i][k]) {
+
+						graph[j][k] = graph[j][i] + graph[i][k];
+						march[j][k] = i;
+					}
+
+				}
+			}
+		}
+		return graph;
+	};
+
 }
